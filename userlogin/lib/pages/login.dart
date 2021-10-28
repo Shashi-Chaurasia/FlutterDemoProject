2728c5cdf9c6748b1d6f59d3e4f401bf1b4b5733
Shashi Chaurasia <57182834+Shashi-Chaurasia@users.noreply.github.com>
2021-10-28 14:32:22 +0530

    changed in button undo

--- userlogin/lib/pages/login.dart
+++ userlogin/lib/pages/login.dart
@@ -71,7 +71,10 @@ class _LoginPageState extends State<LoginPage> {
                         changeButton = true;
                       });
                       await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      setState(() {
+                        changeButton = false;
+                      });
 
                     },
                     child:AnimatedContainer(
