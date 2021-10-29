83868cdce446d19a63922b513befbd6c383d978e
Shashi Chaurasia <57182834+Shashi-Chaurasia@users.noreply.github.com>
2021-10-30 00:03:32 +0530

    add models

---
+++ userlogin/lib/models/catalog.dart
@@ -0,0 +1,23 @@
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+
+  
+
+}
+
+final products  = [
+  Item(
+    "001", 
+    "iphone13 pro" , 
+    "Apple iphone 13 generation", 
+    999, 
+    "#33505a", 
+    "assests/images/mypic.jpeg",)
+];
